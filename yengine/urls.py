f49f2c4f930745cd01c44ba01b3d456be5fabbd2
Pierre Dulac <dulacpier@gmail.com>
2014-11-13 01:12:34 +0100

    Add a media file serving entrypoint for debug mode

--- yengine/urls.py
+++ yengine/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -6,3 +7,11 @@ urlpatterns = patterns('',
     url(r'^api/', include('apps.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
