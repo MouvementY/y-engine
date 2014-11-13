c896027677c019591f92dc3a77d1dbb4a05d2879
Pierre Dulac <dulacpier@gmail.com>
2014-11-13 01:02:11 +0100

    Support multiparts for the signature viewset

--- yengine/apps/api/views.py
+++ yengine/apps/api/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.core.urlresolvers import reverse
 
 from rest_framework import viewsets
+from rest_framework import parsers
 
 from .models import Signature
 from .serializers import SignatureSerializer
@@ -10,3 +11,8 @@ from .serializers import SignatureSerializer
 class SignatureViewSet(viewsets.ModelViewSet):
     queryset = Signature.objects.all()
     serializer_class = SignatureSerializer
+    parser_classes = (
+        parsers.MultiPartParser,
+        parsers.FormParser,
+        parsers.JSONParser
+    )
