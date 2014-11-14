dccede810db3c60518969edd7d92944c01cc5048
Pierre Dulac <dulacpier@gmail.com>
2014-11-14 11:40:32 +0100

    Remove temporarly the authentication

--- yengine/apps/api/views.py
+++ yengine/apps/api/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 
 from rest_framework import viewsets
 from rest_framework import parsers
+from rest_framework.permissions import AllowAny
 
 from .models import Signature
 from .serializers import SignatureSerializer
@@ -16,3 +17,6 @@ class SignatureViewSet(viewsets.ModelViewSet):
         parsers.FormParser,
         parsers.JSONParser
     )
+
+    # TODO implement a proper authentication flow
+    permission_classes = (AllowAny,)
