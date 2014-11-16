24ab850561e5ea5e0003b42c2fef6a9e1461ba90
Pierre Dulac <dulacpier@gmail.com>
2014-11-16 01:34:16 +0100

    Add the api endpoint to register an events subscription

--- yengine/apps/api/urls.py
+++ yengine/apps/api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url, include
 
 from .routers import SecuredDefaultRouter
-from .views import SignatureViewSet
+from .views import (
+    SignatureViewSet,
+    SubscribeToEventNotificationsView)
 
 
 router = SecuredDefaultRouter()
@@ -9,5 +11,6 @@ router.register(r'signatures', SignatureViewSet)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
+    url(r'^events/subscribe/', SubscribeToEventNotificationsView.as_view()),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
--- yengine/apps/api/views.py
+++ yengine/apps/api/views.py
@@ -1,12 +1,17 @@
 from django.views import generic
 from django.core.urlresolvers import reverse
+from django.core.validators import validate_email
 
 from rest_framework import viewsets
 from rest_framework import parsers
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from .models import Signature
 from .serializers import SignatureSerializer
+from .utils import add_email_to_mailchimp
 
 
 class SignatureViewSet(viewsets.ModelViewSet):
@@ -20,3 +25,22 @@ class SignatureViewSet(viewsets.ModelViewSet):
 
     # TODO implement a proper authentication flow
     permission_classes = (AllowAny,)
+
+
+class SubscribeToEventNotificationsView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        email = request.DATA.get('email', None)
+        if not email:
+            return Response({"detail": "Le champ semble si vide..."},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            validate_email(email)
+        except:
+            return Response({"detail": "L'adresse email semble invalide"},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        add_email_to_mailchimp(email)
+        return Response("coucou", status=status.HTTP_201_CREATED)
