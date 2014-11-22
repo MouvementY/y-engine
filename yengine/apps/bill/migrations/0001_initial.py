4a6735276680c09e32ccba62aa7930ffe55a4796
Pierre Dulac <dulacpier@gmail.com>
2014-11-22 01:33:34 +0100

    Reduce migrations to the bare minimal for the sake of simplicity

--- yengine/apps/bill/migrations/0001_initial.py
+++ yengine/apps/bill/migrations/0001_initial.py
@@ -14,17 +14,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Signature',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_updated', models.DateTimeField(auto_now=True)),
                 ('first_name', models.CharField(verbose_name='Prénom', max_length=150)),
                 ('last_name', models.CharField(verbose_name='Nom', max_length=150)),
-                ('email', models.EmailField(unique=True, verbose_name='Email', max_length=75)),
-                ('signature_image', models.ImageField(upload_to=core.utils.UniqueFilename('petition/signatures/%Y/%m/'), blank=True, null=True)),
+                ('email', models.EmailField(verbose_name='Email', max_length=75, unique=True)),
+                ('signature_image', models.ImageField(blank=True, null=True, upload_to=core.utils.UniqueFilename('petition/signatures/%Y/%m/'))),
                 ('signature_image_data_url', models.TextField()),
             ],
             options={
-                'db_table': 'api_signature',
             },
             bases=(models.Model,),
         ),
