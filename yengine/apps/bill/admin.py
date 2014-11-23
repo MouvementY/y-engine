5901f9213bd011169f8cf0b77a2c47c2a95c6184
Pierre Dulac <dulacpier@gmail.com>
2014-11-23 13:34:40 +0100

    Improve the admin model for signature with search capabilities

--- yengine/apps/bill/admin.py
+++ yengine/apps/bill/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from . import models
 
-
-admin.site.register(models.Signature)
+@admin.register(models.Signature)
+class SignatureAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'date_created',
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+        'email',
+    )
