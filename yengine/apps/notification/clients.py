9b1666bfdb34c43308eb262b757f8243029f3a1d
Pierre Dulac <dulacpier@gmail.com>
2014-11-25 02:22:32 +0100

    Add the password to the redis client initialization

--- yengine/apps/notification/clients.py
+++ yengine/apps/notification/clients.py
@@ -12,4 +12,5 @@ PUSH_CHANNEL_SIGNATURE = 'signatures'
 redis_conf = settings.REDIS
 redis_client = redis.StrictRedis(host=redis_conf.hostname,
                                  port=redis_conf.port,
+                                 password=redis_conf.password,
                                  db=0)
