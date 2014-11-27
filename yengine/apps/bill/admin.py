f1fd011d22de8096bd85dc047bcafeafc12e99c2
Pierre Dulac <dulacpier@gmail.com>
2014-11-27 15:52:47 +0100

    Add the signature image to the admin panel

--- yengine/apps/bill/admin.py
+++ yengine/apps/bill/admin.py
@@ -13,3 +13,19 @@ class SignatureAdmin(admin.ModelAdmin):
         'last_name',
         'email',
     )
+    fields = (
+        'first_name',
+        'last_name',
+        'email',
+
+        'signature_image_data_url',
+        '_get_signature_image',
+    )
+    readonly_fields = (
+        '_get_signature_image',
+    )
+
+    def _get_signature_image(self, obj):
+        return "<img src=\"{}\"/>".format(obj.signature_image_data_url)
+    _get_signature_image.allow_tags = True
+    _get_signature_image.short_description = "Signature"
