1e94af041bf16a29cdbde876136e1e9ff152b2b9
Pierre Dulac <dulacpier@gmail.com>
2014-12-06 19:37:52 +0100

    Avoid crashing the signature process in case of redis error

--- yengine/apps/bill/listeners.py
+++ yengine/apps/bill/listeners.py
@@ -1,9 +1,15 @@
+import logging
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+
+from redis.exceptions import ConnectionError
 
 from apps.bill.models import Signature
 from apps.notification.jobs import (
     send_new_signature_notification)
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Signature)
@@ -16,4 +22,7 @@ def handle_signature_is_saved(sender, instance, created, **kwargs):
     if getattr(instance, '_disable_pusher_notification', False):
         return
 
-    send_new_signature_notification(instance)
+    try:
+        send_new_signature_notification(instance)
+    except ConnectionError as exc:
+        logger.error(exc)
