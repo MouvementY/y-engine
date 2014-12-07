d0e9bab16e27ac0ab84946429ca1bd5ac15744bb
Pierre Dulac <dulacpier@gmail.com>
2014-12-07 23:14:05 +0100

    Add the missing timeseries endpoint

--- yengine/apps/api/urls.py
+++ yengine/apps/api/urls.py
@@ -10,9 +10,13 @@ signature_list = SignatureViewSet.as_view({
     'get': 'list',
     'post': 'create',
 })
+signature_timeseries = SignatureViewSet.as_view({
+    'get': 'timeseries',
+})
 
 urlpatterns = patterns('',
     url(r'^signatures/$', signature_list, name='signature-list'),
+    url(r'^signatures/timeseries/$', signature_timeseries, name='signature-timeseries'),
     url(r'^events/subscribe/$', SubscribeToEventNotificationsView.as_view()),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
