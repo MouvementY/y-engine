869d7998b54b500ebf18a3a7fea216b007d5f382
Pierre Dulac <dulacpier@gmail.com>
2014-12-16 01:30:37 +0100

    Add the sanitizer filter to the api serializer validation process

--- yengine/apps/api/serializers.py
+++ yengine/apps/api/serializers.py
@@ -1,12 +1,36 @@
 from rest_framework import serializers
 
 from apps.bill.models import Signature
+from apps.bill.filters import ImageConvertor, DotSignatureIdentifierProcessor
 
 
 class SignatureCreationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Signature
+
+    def validate_signature_image_data_url(self, value):
+        """
+        Avoid getting dot-based signatures or blank ones
+        """
+        if value is None:
+            # already sanitized
+            return None
+
+        # a typical threshold of 1% let us detect the dot-based signatures
+        convertor = ImageConvertor()
+        processor = DotSignatureIdentifierProcessor(threshold=0.01)
+
+        try:
+            img = convertor.image_from_data_uri(value)
+        except ValueError:
+            return None
+
+        ratio = processor.get_colored_pixel_ratio(img)
+        if ratio < processor.threshold:
+            return None
+
+        return value
 
 
 class SignatureSerializer(serializers.ModelSerializer):
